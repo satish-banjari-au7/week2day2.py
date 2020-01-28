6b1e29aac7951c2ca3453b96e0f969932c26ac62
satish_banjari <satishbanjari247@gmail.com>
2020-01-29 02:23:29 +0530

    1st coding challenge

---
+++ week2day2q1.py
@@ -0,0 +1,10 @@
+
+1 mb= 8*1024*1024 = 8,388,608
+1 gb = 8*1024*1024*1024 =8,589,934,592 
+ because
+8 bits = 1 byte
+1024 byts = 1 kilobytes
+1024 kb = 1 mega bytes
+1024 mb = 1 gega bytes
+
+
---
+++ week2day2q4.py
@@ -0,0 +1,7 @@
+
+username = input("Enter your username :")
+password = input("Enter your password :")
+
+print("usename does not exist")
+print("password does not match")
+print("Enter the system,")
